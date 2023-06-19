6d02096b869a9adf27c790768ff58393a131daec
Shubham <shubhamkashyap8004@.com>
2023-06-19 16:18:49 +0530

    first commit

---
+++ src/modules/Landingpage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+export function Myheading() {
+    return <div>
+        <h1>This my first heading</h1>
+        <h2>Hi</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime atque quos debitis, sed earum ab facilis. Laboriosam perferendis consequuntur ipsa cupiditate aperiam quaerat ex corrupti corporis maiores magni. Illum, eaque.</p>
+    </div>
+}
+
+function Mylandingpage() {
+    return "hello This is shubham kashyap";
+}
+
+export default Mylandingpage
+
+
+export function Mylandingpage1() {
+    return "This is page is export";
+}
+
